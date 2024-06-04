bb59386795a54137932456c3513652f114d09339
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-04 10:04:55 +0500

    calbacks

---
+++ callback.js
@@ -0,0 +1,24 @@
+function pro(){
+    console.log("First");
+}
+
+function prop(){
+    console.log("Second");
+}
+
+function prope(){
+    console.log("third");
+}
+
+function proper(){
+    console.log("Fourth");
+}
+
+function propert(){
+    console.log("Fifth");
+}
+pro(()=>{
+    prop(()=>{
+        
+    })
+})
